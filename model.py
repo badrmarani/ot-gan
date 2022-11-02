252207a8c3d84e0973dca195a0c784be8516a4df
badrmarani <badr-eddine.marani@centrale.centralelille.fr>
2022-11-02 21:01:35 +0100

    code formatting

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+
 class CReLU(nn.Module):
     def __init__(self, dim: int = -1) -> None:
         super(CReLU, self).__init__()
@@ -11,12 +12,11 @@ class CReLU(nn.Module):
         return nn.functional.relu(out)
 
     def extra_repr(self) -> str:
-        return 'dim={}'.format(self.dim)
+        return "dim={}".format(self.dim)
+
 
 class Critic(nn.Module):
-    def __init__(self,
-        n_base_channels: int = 3
-        ) -> None:
+    def __init__(self, n_base_channels: int = 3) -> None:
         super(Critic, self).__init__()
         self.net = []
         self.net.append(self._block(n_base_channels, 128, stride=1))
@@ -37,25 +37,25 @@ class Critic(nn.Module):
         out = nn.functional.normalize(out, p=2, dim=1)
         return out
 
+
 class Generator(nn.Module):
-    def __init__(self,
-        n_base_channels: int = 3
-        ) -> None:
+    def __init__(self, n_base_channels: int = 3) -> None:
         super(Generator, self).__init__()
-        self.l1 = nn.Linear(100, 1024*4*4*2)
-        self.conv = nn.Conv2d(128, n_base_channels, kernel_size=(5,5), stride=1, padding=2)
+        self.l1 = nn.Linear(100, 1024 * 4 * 4 * 2)
+        self.conv = nn.Conv2d(
+            128, n_base_channels, kernel_size=(5, 5), stride=1, padding=2
+        )
 
         self.net = []
         self.net.append(self._block(1024))
-        self.net.append(self._block(1024//2))
-        self.net.append(self._block(1024//4))
+        self.net.append(self._block(1024 // 2))
+        self.net.append(self._block(1024 // 4))
         self.net = nn.Sequential(*self.net)
-        
 
     def _block(self, out_size: int):
         return nn.Sequential(
-            nn.Upsample(scale_factor=(2,2), mode="nearest"),
-            nn.Conv2d(out_size, out_size, kernel_size=(5,5), stride=1, padding=2),
+            nn.Upsample(scale_factor=(2, 2), mode="nearest"),
+            nn.Conv2d(out_size, out_size, kernel_size=(5, 5), stride=1, padding=2),
             nn.GLU(dim=1),
         )
 
--- train.py
+++ train.py
@@ -14,7 +14,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 lr = 3e-4
-betas = (.5, .999)
+betas = (0.5, 0.999)
 ngen = 3
 
 batch_size = 32
@@ -22,15 +22,19 @@ epochs = 5
 niter = 10
 eps = 1
 
-transform = torchvision.transforms.Compose([
-    torchvision.transforms.ToTensor(),
-])
+transform = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor(),
+    ]
+)
 
 train_dataset = torchvision.datasets.CIFAR10(
-    root="datasets/", train=True, transform=transform, download=True)
+    root="datasets/", train=True, transform=transform, download=True
+)
 
 train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=2*batch_size, shuffle=True)
+    train_dataset, batch_size=2 * batch_size, shuffle=True
+)
 
 D = Critic().to(device)
 G = Generator().to(device)
@@ -47,8 +51,8 @@ for ep in range(epochs):
     G.train()
     D.train()
     for t, (x, _) in enumerate(train_loader):
-        z = 2*torch.rand(size=(batch_size, 100))-1
-        zp = 2*torch.rand(size=(batch_size, 100))-1
+        z = 2 * torch.rand(size=(batch_size, 100)) - 1
+        zp = 2 * torch.rand(size=(batch_size, 100)) - 1
 
         x, xp = torch.split(x, batch_size)
         x, xp = x.to(device), xp.to(device)
@@ -56,9 +60,9 @@ for ep in range(epochs):
         y, yp = D(G(z)), D(G(zp))
         x, xp = D(x), D(xp)
 
-        if not (t+1) % (ngen+1):
+        if not (t + 1) % (ngen + 1):
             optim_d.zero_grad()
-            loss_d = - SD(x, xp, y, yp, niter, eps)
+            loss_d = -SD(x, xp, y, yp, niter, eps)
 
             loss_d.backward()
             optim_d.step()
@@ -76,26 +80,39 @@ for ep in range(epochs):
             # print(train_loss_G, loss_g.item())
 
         with torch.no_grad():
-            if not (t+1) % 10:
-                    print("""
+            if not (t + 1) % 10:
+                print(
+                    """
                     Epoch: {:^5}/{:^5}| Batch: {:^5}/{:^5} || SD Generator = {:^9.4f} SD Critic = {:^9.4f}
-                    """.format(ep+1, epochs, t+1, len(train_loader), train_loss_G, train_loss_D))
-
+                    """.format(
+                        ep + 1,
+                        epochs,
+                        t + 1,
+                        len(train_loader),
+                        train_loss_G,
+                        train_loss_D,
+                    )
+                )
 
     with torch.no_grad():
         n_samples = 5
 
-        samples = [random.choice(2*torch.rand(size=(200,10,10))-1) for _ in range(n_samples)]
-        out = np.concatenate([
-            # *|CURSOR_MARCADOR|*
-            G(x.view(1,-1).float()).cpu().numpy().transpose((1,2,0)) for x in samples 
-        ], axis=1)
+        samples = [
+            random.choice(2 * torch.rand(size=(200, 10, 10)) - 1)
+            for _ in range(n_samples)
+        ]
+        out = np.concatenate(
+            [
+                # *|CURSOR_MARCADOR|*
+                G(x.view(1, -1).float()).cpu().numpy().transpose((1, 2, 0))
+                for x in samples
+            ],
+            axis=1,
+        )
 
-        plt.figure(figsize=(50,50))
-        plt.imshow((out*255).astype(np.uint8), interpolation="nearest")
+        plt.figure(figsize=(50, 50))
+        plt.imshow((out * 255).astype(np.uint8), interpolation="nearest")
         plt.savefig(f"output-ot-gan-{ep+1}.jpg", dpi="figure")
-    
-
 
 
 # samples = [random.choice(train_dataset.data) for _ in range(n_samples)]
